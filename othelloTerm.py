3931e63bd09a8dc63616b0452536cfc1800381f9
Patrick Chen <chenpatrick1303@gmail.com>
2024-01-22 21:23:55 +0000

    changing the terminal to fix a bug

--- othelloTerm.py
+++ othelloTerm.py
@@ -9,16 +9,16 @@ class terminal(UI):
     def __init__(self, player1, player2):
         super().__init__()
         self.__game = Othello(Player(player1), Player(player2), 1)
-    def twoPlayerGame(self):
-        """
-            Method: twoPlayerGame
-            Parameters: None
-            Returns: None
-            Does: Runs the game for two players
-        """
-        self.__game.setGamemode(2)
-        self.__game.setupGame()
 
+    def game(self, gamemode):
+        '''
+            Method: game
+            Parameters: gamemode (int)
+            Returns: None
+            Does: Runs the game for the given gamemode
+        '''
+        self.__game.setGamemode(gamemode)
+        self.__game.setupGame()
         while not self.__game.checkgameover(self.__game.getBoard()):
             self.__game.displayBoard(self.__game.getBoard())
             choice = input("""
@@ -38,7 +38,7 @@ class terminal(UI):
                         if not valid:
                             print("Invalid move")
                     self.__game.playGame(None, column, row, 1, dir)
-                else:
+                elif self.__game.getGamemode() == 2 and self.__game.getTurn() % 2 == 0:
                     print(f"White ({self.__game.getPlayer2Name()}) turn")
                     valid = False
                     while not valid:
@@ -48,6 +48,11 @@ class terminal(UI):
                         if not valid:
                             print("Invalid move")
                     self.__game.playGame(None, column, row, 2, dir)
+                elif self.__game.getGamemode() == 1 and self.__game.getTurn() % 2 == 0:
+                    print("Computer turn")
+                    move = self.__game.cmove(self.__game.getBoard(), 2)
+                    self.__game.playGame(None, move[0][1], move[0][0], 2, move[1])
+                    self.__game.setTurn(1)
                 self.__game.setTurn(1)
             elif choice == "2":
                 validchoice = False
@@ -56,7 +61,7 @@ class terminal(UI):
                     if c in [1,2,3]:
                         validchoice = True
                     elif c == 4:
-                        return None
+                        print("Cancelled")
                     else:
                         print("Invalid choice, try again")
                 self.__game.saveGame(c)
@@ -67,7 +72,7 @@ class terminal(UI):
                     if c in [1,2,3]:
                         validchoice = True
                     elif choice == 4:
-                        return None
+                        print("Cancelled")
                     else:
                         print("Invalid choice, try again")
                 self.__game.loadGame(c)
@@ -82,81 +87,6 @@ class terminal(UI):
         else:
             print("Tie!")
 
-    def onePlayerGame(self):
-        '''
-            Method: onePlayerGame
-            Parameters: None
-            Returns: None
-            Does: Runs the game for one player and one computer
-        '''
-        self.__game.setGamemode(1)
-        self.__game.setupGame()
-        while not self.__game.checkgameover(self.__game.getBoard()):
-            self.__game.displayBoard(self.__game.getBoard())
-            if self.__game.getTurn() % 2 == 1:
-                print(f"Black ({self.__game.getPlayer1Name()}) turn")
-                choice = input("""
-1. Play
-2. Save
-3. Load
-4. Quit
-                """)
-                if choice == "1":
-                    valid = False
-                    while not valid:
-                        column = int(input("enter a column between 0 and 7: "))
-                        row = int(input("enter a row between 0 and 7: "))
-                        valid, dir = self.__game.isvalidmove(self.__game.getBoard(), column, row, 1)
-                        if not valid:
-                            print("Invalid move")
-                    self.__game.playGame(None, column, row, 1, dir)
-                    self.__game.setTurn(1)
-                elif choice == "2":
-                    self.__game.saveGame()
-                elif choice == "3":
-                    self.__game.loadGame()
-                elif choice == "4":
-                    return None
-            else:
-                print("Computer turn")
-                move = self.__game.getValidMoves(self.__game.getBoard(), 2)
-                #print(self.__game.getDifficulty())
-                if self.__game.getDifficulty() == 1:
-                    #choose a random move from the list of valid moves
-                    computermove = random.choice(move)
-                if self.__game.getDifficulty() == 2:
-                    #choose the move which flips the most pieces
-                    same = copy.deepcopy(self.__game.getBoard())
-                    print(same)
-                    maxflips = 0
-                    computermove = random.choice(move)
-                    for mov in move:
-                        curr_score = self.__game.getWhiteScore(same)
-                        self.__game.playGame(same, mov[0][0], mov[0][1], 2, mov[1])
-                        score_diff = self.__game.getWhiteScore(same) - curr_score
-                        if score_diff > maxflips:
-                            maxflips = score_diff
-                            computermove = mov
-                        
-                if self.__game.getDifficulty() == 3:
-                    computermove, score = self.__game.minimax(self.__game.getBoard(), 3, True, 2, -100000, 100000)
-                    print(f"minimax score: {score}")
-
-                if self.__game.getDifficulty() == 4:
-                    computermove, score = self.__game.minimax(self.__game.getBoard(), 5, True, 2, -100000, 100000)
-                    print(f"minimax score: {score}")
-                self.__game.playGame(None, computermove[0][1], computermove[0][0], 2, computermove[1])
-                self.__game.setTurn(1)
-
-        white, black = self.__game.calculateWinner()
-        print(f"Black: {white}\nWhite: {black}")
-        if white > black:
-            print(f"{self.__game.getPlayer1Name()} wins!")
-        elif black > white:
-            print(f"Computer wins!")
-        else:
-            print("Tie!")
-
     def run(self):
         '''
             Method: startGame
@@ -166,17 +96,10 @@ class terminal(UI):
         '''
         x = input("1. 1 Player Game\n2. 2 Player Game\n3. Load Game\n4. Quit\n")
         if x == "2":
-            self.twoPlayerGame()
+            self.game(2)
         elif x == "1":
-            self.onePlayerGame()
+            self.game(1)
         elif x == "3":
             self.__game.loadGame()
-            if self.__game.getGamemode() == 1:
-                self.onePlayerGame()
-            elif self.__game.getGamemode() == 2:
-                self.twoPlayerGame()
-            else:
-                print("Invalid gamemode")
-            return None
         elif x == "4":
             return None
